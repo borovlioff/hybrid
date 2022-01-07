64e8fbb007b1cdb5b2d84a73ac3d789998bde382
borovlioff <borovliov.alex@ya.ru>
2022-01-07 17:22:12 +0700

    add rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import { uglify } from "rollup-plugin-uglify";
+import replace from 'rollup-plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy'
+import svelte from 'rollup-plugin-svelte';
+import autoPreprocess from 'svelte-preprocess';
+
+import vue from 'rollup-plugin-vue'
+
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/app.ts',
+    output: {
+      dir: 'rollup-dist',
+      format: 'es',
+    },
+    plugins: [
+        copy({
+            targets: [{src : 'src/index.html' ,  dest : 'rollup-dist'}]
+        }),
+        svelte({
+            preprocess: autoPreprocess()
+        }),
+        vue(/* options */)
+        ,
+        typescript(),
+        babel({ babelHelpers: 'bundled' }),
+        commonjs(),
+        nodeResolve(),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify( 'production' )
+		}),
+        uglify(),
+    ]
+  };
